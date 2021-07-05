ac187dc0dd2051c7eee8e8c165d5bd39b0b32a33
kissesy <39688948+kissesy@users.noreply.github.com>
2021-07-05 18:08:34 +0900

    MenubarBtn

---
+++ main.js
@@ -0,0 +1,20 @@
+const MenuBarBtn = document.querySelector(".navbar__bar")
+const NavbarMenu = document.querySelector(".navbar__menu")
+
+function open_menu(event) {
+    if (NavbarMenu.style.display == "inherit") {
+        NavbarMenu.style.display = "none"
+        console.log("asd")
+    } else {
+        NavbarMenu.style.display = "inherit"
+    }
+
+
+}
+
+function init() {
+    MenuBarBtn.addEventListener("click", open_menu);
+
+}
+
+init()
