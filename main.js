a1d1d07915eff60573d22a966ff3cc80409f7ccb
tuuna <b7712983@naver.com>
2023-12-11 23:36:50 +0900

    123

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 
+const post_container = document.querySelector("#main");
+const content_container = document.querySelector("#content");
+
+const md = window.markdownit();
+
 function load_post(posts)
 {
     list = document.querySelector("#main");
 
     posts.forEach(post => {
         let container = document.createElement("div"); 
-        let a = document.createElement("a"); 
+        let a = document.createElement("span"); 
         let inner = document.createElement("div");
 
         let category = document.createElement("p");
         let date = document.createElement("p");
 
-        a.setAttribute("href", post['link']);
+        a.addEventListener("click", function(){
+            update_content(post['link'])
+        })
         a.innerText = post['title'];
         category.innerText = post['category'];
         date.innerText = post['date'];
@@ -29,6 +36,20 @@ function load_post(posts)
     });
 }
 
+function update_content(link)
+{
+    post_container.classList.toggle("vinish");
+    content_container.classList.toggle("vinish");
+
+    fetch(link)
+    .then((res) => {
+        return res.text();
+    })
+    .then((obj) => {
+        let b = md.render(obj);
+        content_container.innerHTML = b;
+    })
+}
 
 function init()
 {
